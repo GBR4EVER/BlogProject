69252242ad05e87a53e3c551c4da049b026daa95
Corey <burkleycorey@yahoo.com>
2014-10-15 02:07:13 -0500

    Added Placeholder HTML

--- themes/TravelBlog/index.php
+++ themes/TravelBlog/index.php
@@ -1,5 +1,41 @@
 <?php get_header(); ?>
 
-
+    <div class="row">
+      
+      <div class="large-8 columns">
+          
+          <div class="panel">
+          
+          <h2>Welcome to Dan and Liz's Travel Blog!</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          
+          <a class="button round" href="#">Find out more</a>
+        
+        </div>
+      
+      </div>
+      
+      <div class="large-4 columns">
+        
+        <img src="img/New-Zealand-Vacation1.jpg" />
+      
+      </div>
+      
+    </div>
+    
+    <hr />
+    
+    <div class="row">
+      
+      <div class="large-12 columns">
+        
+        <h3>We love to travel.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        
+      </div>
+    
+    </div>
+    
+    <hr />
 
 <?php get_footer();
