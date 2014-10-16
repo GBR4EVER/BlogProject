ad4d57521829149c9cb608af6a0dd3e223496107
Corey <burkleycorey@yahoo.com>
2014-10-16 07:03:27 -0500

    Added more styling

--- themes/TravelBlog/footer.php
+++ themes/TravelBlog/footer.php
@@ -1,4 +1,11 @@
+  <script src="js/vendor/jquery.js"></script>
+  <script src="js/foundation/foundation.js"></script>
+  <script src="js/foundation/foundation.topbar.js"></script>
+    <!-- Other JS plugins can be included here -->
 
+  <script>
+    $(document).foundation();
+  </script>
 	<?php wp_footer(); ?>
 </body>
 </html>
--- themes/TravelBlog/header.php
+++ themes/TravelBlog/header.php
@@ -21,14 +21,12 @@
 
 <body>
 
-    <nav class="top-bar">
-    
-      <section class="top-bar-section">
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title area">
+        <li class="name"><h1><a href="index.html">Far & Wide</a></h1></li>
+      </ul>
       
-        <ul class="title area">
-          <li class="name"><h1><a href="index.html">NewZealand</a></h1></li>
-        </ul>
-    
+      <section class="top-bar-section">
         <ul class="right">
           <li><a href="#">About</a></li>
           <li><a href="#">Posts</a></li>
